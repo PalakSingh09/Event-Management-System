6e41feaab929d2c0e6f3ce585d4997b20264fe24
PalakSingh09 <palaksingh2005bpl@gmail.com>
2026-04-30 17:24:19 +0530

    Add files via upload
    Initial commit - Event Management System

---
+++ DBConnection.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/eventdb";
+            String user = "root";
+            String password = "root"; // change if needed
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, user, password);
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
